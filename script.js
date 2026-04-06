2d04ddb418950a45dda333183a52ca409514a2b3
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-06 21:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementById('enterBtn');
+let curr = document.getElementById('status');
+
+function enterMeta(event){
+	console.log("Entered Metaverse");
+	curr.innerHTML = "<h1>Entered Metaverse</h1>";
+}
+btn.addEventListener('click',enterMeta);
